22b7d322f4d2f106c6214e51d4676c3e7eacecf8
Remigiusz Oczko <remigiusz.oczko@gmail.com>
2023-03-11 20:34:30 +0100

    starting hangman function

--- Project_2/src.py
+++ Project_2/src.py
@@ -8,3 +8,16 @@ def get_random_word(words):
         random_word = random.choice(words)
     return random_word.upper()
 
+def hangman():
+    word=get_random_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercases)
+    used_letters = set()
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter
+
+
+user_input = input("Type a letter ")
+print(user_input)
